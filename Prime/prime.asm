80a7750ce929333f1fa9ff8e90ebcd716e90a285
Carla de Beer <carla.debeer.uk@gmail.com>
2014-11-24 12:38:13 +0000

    Update prime.asm

--- Prime/prime.asm
+++ Prime/prime.asm
@@ -16,10 +16,10 @@ prime:
   mov	rbp, rsp
   
   mov 	r15, rdi
-  cmp	r15, 0		; if equal to 0
+  cmp	r15, 0			; if equal to 0
   je 	return_false
   
-  cmp 	r15, 1		; if equal to 1
+  cmp 	r15, 1			; if equal to 1
   je 	return_false
   
   xor 	rax, rax
@@ -33,7 +33,7 @@ while:
   jnl 	end_while
   
   idiv 	r14
-  cmp 	rdx, 0		; number is a factor
+  cmp 	rdx, 0			; number is a factor
   je 	return_false
   xor 	rax, rax
   xor 	rdx, rdx
@@ -57,15 +57,15 @@ while:
 	  ret
   
 end_while:
-	xor 	rax, rax
-	xor 	rdi, rdi
-	xor 	rsi, rsi  
-	lea 	rdi, [isPrime]
-	mov 	rsi, r15
+	 xor 	rax, rax
+  	 xor 	rdi, rdi
+	 xor 	rsi, rsi  
+	 lea 	rdi, [isPrime]
+	 mov 	rsi, r15
   
-	xor eax, eax  
-	call printf  
-	xor eax, eax
+	 xor	eax, eax  
+	 call	printf  
+	 xor 	eax, eax
   
 	leave
 	ret
