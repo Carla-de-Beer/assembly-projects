02d9ea7a267eac535091102f8c55c1371b351db7
Carla de Beer <carla.debeer.uk@gmail.com>
2014-11-24 13:22:26 +0000

    New files added

---
+++ Triangle area calculator/area.asm
@@ -0,0 +1,47 @@
+; Carla de Beer
+; Assembly program that inputs the area of a triangle based 
+; on the input of a a set of three 2D coordinates passed via the C function
+; November 2014
+
+segment .data
+  half 		dd 	0.5  
+  zero 		dd 	0.00
+  negative 	dd 	-1.0
+
+segment .text
+  global area
+
+area:
+
+  push 	rbp
+  mov 	rbp, rsp
+  
+  movss xmm7, xmm0
+  subss xmm7, xmm2
+  
+  movss xmm8, xmm1
+  subss xmm8, xmm3
+  
+  movss xmm9, xmm2
+  subss xmm9, xmm4
+  
+  movss xmm10, xmm3
+  subss xmm10, xmm5
+  
+  mulss xmm7, xmm10
+  mulss xmm8, xmm9
+  subss xmm7, xmm8
+  
+  ucomiss xmm7, [zero]
+  jae get_result
+    mulss xmm7, [negative]
+  
+  get_result:
+ 
+  mulss xmm7, [half] 
+  movss xmm0, [zero]
+  movss xmm0, xmm7
+  
+  leave
+  ret
+	
---
+++ Triangle area calculator/areaCalc.c
@@ -0,0 +1,16 @@
+/* Assembly program, called via C, that calculates the area of a triangle 
+ * based on a set of three 2D input coordinates. */
+
+#include <stdio.h>
+
+extern float area(float x1, float y1, float x2, float y2, float x3, float y3);
+
+int main()
+{
+	float ans = area(3.00,5.00,4.00,0.00,1.00,2.00);  	//6.5000000000
+	//float ans = area(0.00,0.00,5.00,0.00,5.00,12.00);  	//30.0000000000
+	//float ans = area(0.00,0.00,3.00,0.00,3.00,4.00);  	//6.0000000000
+	//float ans = area(0.01,0.07,3.03,0.02,3.08,4.01);      //6.0261502266
+	printf("%.10f\n",ans);	
+	return 0;
+}
